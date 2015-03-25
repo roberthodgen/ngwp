a45ac5996701e39c7c384556502c1135fb7a135c
Robert Hodgen <roberthodgen@gmail.com>
2015-03-25 15:25:29 -0400

    added grunt connect; changed indents

--- Gruntfile.js
+++ Gruntfile.js
@@ -5,30 +5,40 @@ module.exports = function(grunt) {
 
 	// Project configuration.
 	grunt.initConfig({
-	pkg: grunt.file.readJSON('package.json'),
-	uglify: {
-		options: {
-		banner: '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> */\n'
-		},
-		build: {
-		src: 'src/<%= pkg.name %>.js',
-		dest: 'dist/<%= pkg.name %>.min.js'
+		pkg: grunt.file.readJSON('package.json'),
+		uglify: {
+			options: {
+			banner: '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> */\n'
+			},
+			build: {
+			src: 'src/<%= pkg.name %>.js',
+			dest: 'dist/<%= pkg.name %>.min.js'
+			}
+		}, jshint: {
+			files: ['Gruntfile.js', 'src/**/*.js'],
+			options: {
+			// options here to override JSHint defaults
+			globals: {
+				angular: true,
+				jQuery: true,
+				console: true,
+				module: true,
+				document: true
+			}
+			}
+		}, connect: {
+			server: {
+				options: {
+					hostname: "localhost",
+					port: 8081,
+					base: 'dist',
+					keepalive: true
+				}
+			}
 		}
-	}, jshint: {
-		files: ['Gruntfile.js', 'src/**/*.js'],
-		options: {
-		// options here to override JSHint defaults
-		globals: {
-			angular: true,
-			jQuery: true,
-			console: true,
-			module: true,
-			document: true
-		}
-		}
-	}
 	});
 
 	grunt.loadNpmTasks('grunt-contrib-uglify');
 	grunt.loadNpmTasks('grunt-contrib-jshint');
+	grunt.loadNpmTasks('grunt-contrib-connect');
 };
