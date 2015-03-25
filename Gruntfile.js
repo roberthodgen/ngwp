7f5ba1bfa4c2690350fcbd6efd872b8f02273f4b
Robert Hodgen <roberthodgen@gmail.com>
2015-03-25 18:17:32 -0400

    Gruntfile imporvements

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,18 +1,40 @@
 module.exports = function(grunt) {
 	// Do grunt-related things in here
 
-	grunt.registerTask('default', ['uglify', 'jshint']);
+	// Load Grunt tasks
+	grunt.loadNpmTasks('grunt-contrib-uglify');
+	grunt.loadNpmTasks('grunt-contrib-jshint');
+	grunt.loadNpmTasks('grunt-contrib-connect');
+	grunt.loadNpmTasks('grunt-contrib-clean');
+	grunt.loadNpmTasks('grunt-contrib-copy');
+	grunt.loadNpmTasks('grunt-open');
 
 	// Project configuration.
 	grunt.initConfig({
 		pkg: grunt.file.readJSON('package.json'),
-		uglify: {
+		meta: {
+			banner: '/**\n' +
+				' * <%= pkg.name %>\n' +
+				' * \n' +
+				' * @version v<%= pkg.version %> - <%= grunt.template.today("yyyy-mm-dd") %>\n' +
+				' * @link <%= pkg.site %>\n' +
+				' * @author <%= pkg.author.name %> <<%= pkg.author.email %>>\n' +
+				// ' * @license <%= pkg.license %>\n' +
+				' */\n'
+		}, uglify: {
 			options: {
-				banner: '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> */\n'
+				banner: '<%= meta.banner %>'
 			},
 			build: {
-				src: 'src/<%= pkg.name %>.js',
-				dest: 'dist/<%= pkg.name %>.min.js'
+				files: {
+					'dist/ngwp/ngwp.min.js': ['src/ngwp/ngwp.js'],
+
+					'dist/ngwp/common/api-endpoint.min.js': ['src/ngwp/common/api-endpoint.js'],
+					'dist/ngwp/common/filters.min.js': ['src/ngwp/common/filters.js'],
+
+					'dist/ngwp/controllers/root-ctrl.min.js': ['src/ngwp/controllers/root-ctrl.js'],
+
+				}
 			}
 		}, jshint: {
 			files: ['Gruntfile.js', 'src/**/*.js'],
@@ -35,10 +57,33 @@ module.exports = function(grunt) {
 					keepalive: true
 				}
 			}
+		}, clean: {
+			build: 'dist/*',
+			release: 'dist/*'
+		}, open: {
+			server: {
+				path: 'http://localhost:8081'
+			}
+		}, copy: {
+			dist: {
+				files: [{
+					expand: true,
+					cwd: 'src',
+					src: '**/*',
+					dest: 'dist',
+					filter: 'isFile'
+				}]
+			}
 		}
 	});
 
-	grunt.loadNpmTasks('grunt-contrib-uglify');
-	grunt.loadNpmTasks('grunt-contrib-jshint');
-	grunt.loadNpmTasks('grunt-contrib-connect');
+	// Grunt Tasks
+	grunt.registerTask('default', ['jshint']);
+	grunt.registerTask('build', ['copy', 'uglify']);
+	grunt.registerTask('run', ['open', 'connect']);
+
+	// Shortcuts
+	grunt.registerTask('b', 'build');
+	grunt.registerTask('c', 'clean');
+	grunt.registerTask('r', 'run');
 };
