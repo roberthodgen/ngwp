92bc6337fcb80cf9637cc76ecafeb369891fc8fe
Robert Hodgen <roberthodgen@gmail.com>
2015-03-25 22:46:39 -0400

    use blog resolve

--- src/ngwp/controllers/blog-ctrl.js
+++ src/ngwp/controllers/blog-ctrl.js
@@ -4,6 +4,7 @@
 
 	app.controller('blogCtrl', ['$scope', 'blog', function($scope, blog) {
 		$scope.init = function() {
+			console.log('[blogCtrl] $scope.init(): Called.');
 			$scope.blog = blog;
 		};
 
