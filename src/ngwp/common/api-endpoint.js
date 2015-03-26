8480e587c6a30f1c97178a2a49035a10dcb446d4
Robert Hodgen <roberthodgen@gmail.com>
2015-03-25 20:55:12 -0400

    Updated proper URI

--- src/ngwp/common/api-endpoint.js
+++ src/ngwp/common/api-endpoint.js
@@ -7,12 +7,12 @@
 	*	Defines the API_ENDPOINT contant.
 	*
 	*	API_ENDPOINT defines the URL (or URI) of the Wordpress REST API;
-	*	the endpoint may live on this domain (e.g. `/wp-api/`);
-	*	or reside on a remote server (e.g. `https://api.example.com/wp-api/`);
+	*	the endpoint may live on this domain (e.g. `/wp-json/`);
+	*	or reside on a remote server (e.g. `https://api.example.com/wp-json/`);
 	*	please verify CORS settings for remote server endpoint!
 	*	NOTE: Should always end in trailing slash!
 	*/
 
-	app.constant('API_ENDPOINT', '/wp-api/');
+	app.constant('API_ENDPOINT', '/wp-json/');
 
 })();
