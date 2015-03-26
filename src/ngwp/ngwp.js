35fcc08c6ffad1fa20daa95bcec73e0345a61636
Robert Hodgen <roberthodgen@gmail.com>
2015-03-25 22:32:30 -0400

    add more

--- src/ngwp/ngwp.js
+++ src/ngwp/ngwp.js
@@ -1,5 +1,42 @@
 (function() {
 
-	var app = agnular.module('ngwp', []);
+	var app = angular.module('ngwp', [
+		'ui.router',		// Angular UI Router via https://github.com/angular-ui/ui-router
+		'ngwp.rootCtrl',
+		'ngwp.blogCtrl',
+		'ngwp.apiService'
+	]);
+
+	app.config(['$stateProvider', function($stateProvider) {
+
+		$stateProvider.state('main', {
+			url: '/',
+			templateUrl: '/ngwp/templates/main.html',
+			controller: 'blogCtrl',
+			resolve: {
+				blog: ['apiService', function(apiService) {
+					return apiService.fetchBlog().then(function(response) {
+						if (!response.error) {
+							return response;
+						} else {
+							alert('Could not load blog.');
+							return null;
+						}
+					});
+				}]
+			}
+		});
+
+		$stateProvider.state('test', {
+			url: '/test',
+			templateUrl: '/ngwp/templates/main.html'
+		});
+
+	}]);
+
+	// Enable HTML5-mode for $locationProvider
+	app.config(['$locationProvider', function($locationProvider) {
+		$locationProvider.html5Mode(true);
+	}]);
 
 })();
