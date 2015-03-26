acbe868d3c6c78e789425e6ff30f2ec5f129901f
Robert Hodgen <roberthodgen@gmail.com>
2015-03-26 12:06:06 -0400

    api server test

--- src/ngwp/common/api-endpoint.js
+++ src/ngwp/common/api-endpoint.js
@@ -6,7 +6,7 @@
 	/*
 	*	Defines the API_ENDPOINT contant.
 	*
-	*	API_ENDPOINT defines the URL (or URI) of the Wordpress REST API;
+	*	API_ENDPOINT defines the URL (or URI) of the WordPress REST API;
 	*	the endpoint may live on this domain (e.g. `/wp-json/`);
 	*	or reside on a remote server (e.g. `https://api.example.com/wp-json/`);
 	*	please verify CORS settings for remote server endpoint!
---
+++ src/ngwp/common/api-service.js
@@ -0,0 +1,36 @@
+(function() {
+
+	var app = angular.module('ngwp.apiService', ['ngwp.apiEndpoint']);
+
+	app.service('apiService', ['$http', 'API_ENDPOINT', function($http, API_ENDPOINT) {
+
+		this.fetchBlog = function() {
+			return $http({
+				method: 'GET',
+				url: API_ENDPOINT
+			}).then(function(response) {
+				// HTTP 200-299 Status
+				if (angular.isObject(response.data) && response.status === 200) {
+					// Success
+					console.log('[apiService] fetchBlog(): Fetch success.');
+					return response.data;
+				} else {
+					// Error
+					console.log('[apiService] fetchBlog(): Error reading response.');
+					return {
+						error: true
+					};
+				}
+			}, function(response) {
+				// Error
+				console.log('[apiService] fetchBlog(): Request error: '+response.status);
+				return {
+					error: true,
+					status: response.status
+				};
+			});
+		};
+
+	}]);
+
+})();
