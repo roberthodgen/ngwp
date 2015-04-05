39fc30ed92effb11027e856331c7e65796f0f6da
Robert Hodgen <roberthodgen@gmail.com>
2015-04-05 17:48:48 -0400

    will return a promise for a cached template

--- src/ngwp/common/state-factory.js
+++ src/ngwp/common/state-factory.js
@@ -20,14 +20,14 @@
 
 					templateFactory(futureState.template).then(function(response) {
 						// HTTP 200-299 Status
-						templateDeferred.resolve(response.data);
+						templateDeferred.resolve(response);
 					}, function(response) {
 						// Error
 
 						// Try loading the gemeric...
 						templateFactory('archive').then(function(response) {
 							// HTTP 200-299 Status
-							templateDeferred.resolve(response.data);
+							templateDeferred.resolve(response);
 						}, function(response) {
 							// Error
 							templateDeferred.resolve('<h1>Template not found.</h1>');
@@ -38,12 +38,20 @@
 					// return templateFactory(futureState.template);
 				}, controllerProvider: function(controllerForTemplateFactory) {
 					return controllerForTemplateFactory(futureState.template);
-				}, resolve: {
-					posts: ['apiService', function(apiService) {
-						return apiService.fetchPosts();
-					}]
-				}
+				}, resolve: {}
 			};
+
+			if (futureState.template.lastIndexOf('single', 0) === 0) {
+				// Single `post`
+				fullState.resolve.post = ['apiService', function(apiService) {
+					return apiService.fetchPostBySlug('hello-world');
+				}];
+			} else if (futureState.template.lastIndexOf('archive', 0) === 0) {
+				// Multiple `posts`
+				fullState.resolve.posts = ['apiService', function(apiService) {
+					return apiService.fetchPosts();
+				}];
+			}
 
 			// Resolve the full state;
 			// can be done asynchronously
@@ -57,23 +65,30 @@
 	app.constant('TEMPLATE_DIRECTORY', '/ngwp/templates/');
 
 
-	app.factory('templateFactory', ['$http', '$templateCache', 'TEMPLATE_DIRECTORY', function($http, $templateCache, TEMPLATE_DIRECTORY) {
+	app.factory('templateFactory', ['$http', '$q', '$templateCache', 'TEMPLATE_DIRECTORY', function($http, $q, $templateCache, TEMPLATE_DIRECTORY) {
 		return function(template) {
-
-			// return TEMPLATE_DIRECTORY + template + '.html';
+			var deferred = $q.defer();
 
 			// Attempt loading the FULL template
 			var cached_template = $templateCache.get(TEMPLATE_DIRECTORY + template + '.html');
 
 			if (!!cached_template) {
-				return cached_template;
+				console.log('returning cached template.');
+				deferred.resolve(cached_template);
+				return deferred.promise;
 			}
 
-			return $http({
+			$http({
 				method: 'GET',
 				url: TEMPLATE_DIRECTORY + template + '.html',
 				cache: $templateCache
+			}).then(function(response) {
+				deferred.resolve(response.data);
+			}, function(response) {
+				deferred.reject(false);
 			});
+
+			return deferred.promise;
 		};
 	}]);
 
