4332246a68de9c651cdc44efbce11338fa594dda
Robert Hodgen <roberthodgen@gmail.com>
2015-04-05 12:47:50 -0400

    cleanup

--- src/ngwp/ngwp.js
+++ src/ngwp/ngwp.js
@@ -73,8 +73,8 @@
 		$futureStateProvider.addResolve(loadAndRegisterFutureStates);
 	}]);
 
-	app.config(['$stateProvider', function($stateProvider) {
 
+	app.config(['$stateProvider', function($stateProvider) {
 		$stateProvider.state({
 			name: 'test',
 			url: '/test',
@@ -88,80 +88,16 @@
 				});
 			}
 		});
-		
-
 	}]);
 
-	var controllerForTemplate = function(string) {
-		if (string.lastIndexOf('single', 0) === 0) {
-			return 'singleCtrl';
-		} else if (string.lastIndexOf('archive', 0) === 0) {
-			return 'archiveCtrl';
-		}
-	};
 
-	// var loadingRoutes = 0;
-
-
-	// app.config(['$routeProvider', function($routeProvider) {
-
-	// 	// 404 Error
-	// 	$routeProvider.when('/404', {
-	// 		templateUrl: '/ngwp/templates/404.html'
-	// 	});
-
-	// 	$routeProvider.when('/sample-page', {
-	// 		templateUrl: '/ngwp/templates/single.html',
-	// 		controller: 'singleCtrl',
-	// 		resolve: {
-	// 			post: ['pageFactory', function(pageFactory) {
-	// 				return pageFactory.forEndpoint('pages/sample-page');
-	// 			}]
-	// 		}
-	// 	});
-
-	// 	$routeProvider.otherwise({
-	// 		resolve: {
-	// 			dependencies: ['$q', '$location', '$http', '$route', 'apiService', 'pageFactory', function($q, $location, $http, $route, apiService, pageFactory) {
-	// 				console.log('resolve dependencies');
-	// 				var deferred = $q.defer();
-	// 				var path = $location.path();
-
-	// 				if (loadingRoutes < 2) {
-	// 					apiService.fetchRoutes().then(function(routes) {
-
-	// 						for (var i = routes.length - 1; i >= 0; i--) {
-								
-	// 							var templateUrl = '/ngwp/templates/' + routes[i].template + '.html';
-	// 							console.log('templateUrl: '+templateUrl);
-	// 							var controller = controllerForTemplate(routes[i].template);
-	// 							console.log('controller: '+controller);
-	// 							$routeProvider.when(routes[i].url, {
-	// 								templateUrl: templateUrl,
-	// 								controller: controller,
-	// 								resolve: {
-	// 									post: pageFactory.forRoute(routes[i])
-	// 								}
-	// 							});
-	// 						}
-
-	// 						loadingRoutes++;
-	// 						console.log('$routeScope.$broadcast with $locationChangeSuccess');
-	// 						// $rootScope.$apply(function() {
-	// 							deferred.resolve();
-	// 						// });
-	// 						$route.reload();
-	// 						// $rootScope.$broadcast('$locationChangeSuccess', path, path);
-	// 					});
-	// 				}
-	// 				return deferred.promise;
-	// 			}]
-	// 		}, controller: function($scope) {
-	// 			console.log('$routeProvider.otherwise controller instantiated');
-	// 		}, template: '<div></div>'
-	// 	});
-
-	// }]);
+	// var controllerForTemplate = function(string) {
+	// 	if (string.lastIndexOf('single', 0) === 0) {
+	// 		return 'singleCtrl';
+	// 	} else if (string.lastIndexOf('archive', 0) === 0) {
+	// 		return 'archiveCtrl';
+	// 	}
+	// };
 
 
 	// Enable HTML5-mode for $locationProvider
